68ca98dee252d9b60d81e3892e4d3fc3f91b0801
NirajTharu <nirajc279@gmail.com>
2025-08-07 15:51:08 +0545

    Modifying the array file

--- Array.java
+++ Array.java
@@ -1,3 +1,12 @@
 public class Array {
-    
+    public static void main(String[]args)
+    {
+        char alphbet[]={'A','B','C','D','E','F'};
+        System.out.println(alphbet[0]);
+        // For printing all the array elements using for loop
+        for(int i=0;i<alphbet.length;i++)
+            {
+                 System.out.println(alphbet[i]);
+            }
+    }
 }
